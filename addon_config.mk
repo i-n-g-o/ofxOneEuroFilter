7919de0be6b58537f3778c1f76c3ec15c926bb21
Ingo Randolf <ingo.randolf@servus.at>
2015-08-10 09:48:03 +0200

    add addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxOneEuroFilter
+	ADDON_DESCRIPTION = Adaption of 1€ Filter by Géry Casiez, Nicolas Roussel and Daniel Vogel
+	ADDON_AUTHOR = Ingo Randolf
+	ADDON_TAGS = "Machine Learning"
+	ADDON_URL = https://github.com/i-n-g-o/ofxOneEuroFilter
